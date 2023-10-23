8d87ad2cb90283bacf2b26013ebfba3784d794c4
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2023-10-23 17:16:25 +0700

    menambahkan init, named, dan redirect constractor

--- dart_oop/bin/init_formal_parameter.dart
+++ dart_oop/bin/init_formal_parameter.dart
@@ -4,20 +4,11 @@ class Person {
   final String country = "Indonesia";
 
   Person(this.name, this.address);
-
-  Person.withName(this.name);
-
-  Person.withAddress(this.address);
 }
 
 void main() {
   var person = Person("Asep", "Bandung");
-  var person2 = Person.withName("Jajang");
-  var person3 = Person.withAddress("Jakarta");
 
   print(person.name);
   print(person.address);
-
-  print(person2.name);
-  print(person3.address);
 }
